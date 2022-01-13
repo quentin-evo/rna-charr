8199a787d989210188aac359d888732ba6aeefab
quentin-evo <83973233+quentin-evo@users.noreply.github.com>
2022-01-13 09:28:37 +0000

    Update GOscript.R

--- GOscript.R
+++ GOscript.R
@@ -1,62 +1,19 @@
 library(topGO)
-set1 <- read.table("~/Documents/Analyses/Post-zygotic mechanisms/RNAseq/mRNA/DE-analyses/Go-analysis/geneids.txt", h=F)
+set1 <- read.table("~/geneids.txt", h=F) ### Import textfile of gene IDs identified during DE analyses (see DE script)
 colnames(set1) <- c("ID","Gene.name")
 
 
 main.set <- set1$ID[ set1$Gene.name %in% row.names(dds)]
 
 
-canada <- readMappings(file = "./Desktop/Databases_GO/geneidgo.db") # Import mapped GOterm
+canada <- readMappings(file = "./geneidgo.db") # Import mapped GOterm (from the Salvelinus genome assembly ASM291031v2)
 
-
-##### GO of differentially expressed genes at 200ts
-
-GOlist.de <- list()
-de.genes <- c(sh.pl.sb.200, sh.plsb.plpl.200, sh.sbpl.plpl.200, sh.plsb.sbsb.200,
-              sh.sbpl.sbsb.200, sh.plsb.sbpl.200)
-de.nm <- c("SBxSB/PLxPL-200ts", "PLxSB/PLxPL-200ts", "SBxPL/PLxPL-200ts", "PLxSB-SBxSB-200ts",
-              "SBxPL/SBxSV-200ts", "PLxSB/SBxPL-200ts")
-
-
-# Produce GO list (the object "contr" is from the DE.R manuscript)
-for(i in 1:length(names(contr))){
-  sel.set <-set1$ID[set1$Gene.name %in% rownames(na.omit(contr[[i]])
-                                                 [na.omit(contr[[i]]$padj) < 0.1,])]
-  
-  geneList <- factor(as.integer(main.set %in% sel.set))
-  names(geneList) <- main.set
-  
-  tGOdata <- new("topGOdata",
-                 description = names(contr)[i],
-                 ontology = "BP",
-                 allGenes = geneList, 
-                 nodeSize = 10,
-                 annot = annFUN.gene2GO, gene2GO = canada)
-  wtest <- runTest(tGOdata, algorithm = "weight01", statistic = "fisher")
-  
-  gt.de1 <- GenTable(tGOdata, weight01Fisher = wtest, 
-                     orderBy = "weight01Fisher", 
-                     ranksOf = "weight01Fisher",topNodes = 10)
-  GOlist.de[[i]] <- gt.de1
-}
-
-for(i in 1:length(names(contr))){
-  GOlist.de[[i]]$contrast <- rep(names(contr)[i], nrow(GOlist.de[[i]])) 
-}
-
-dfGO <- do.call(rbind,GOlist.de)
-
-write.table(dfGO, "~/Documents/Analyses/Post-zygotic mechanisms/RNAseq/mRNA/DE-analyses/Go-analysis/go-DE.txt")
-
-
-showSigOfNodes(tGOdata, score(ftest), firstSigNodes = 5, useInfo = 'all')
-showSigOfNodes(tGOdata, score(ftest2), firstSigNodes = 5, useInfo = 'all')
 
 
 
 #### targets of differentially expressed miRNAs
 
-mi.de.list <- readRDS("~/Documents/Analyses/Post-zygotic mechanisms/RNAseq/mRNA/miRNA-target/de.list.miRNA.RDS")
+mi.de.list <- readRDS("~/de.list.miRNA.RDS") # ID of differentially expressed miRNAs (from the miRNA-de.R script)
 
 mi.de.names<- c("sh-pl-sb-150","sh-plsb-plpl-150", "sh-sbpl-plpl-150" ,"sh-plsb-sbsb-150",
              "sh-sbpl-sbsb-150", "sh-pl-sb-200","sh-plsb-plpl-200", "sh-sbpl-plpl-200", 
@@ -99,7 +56,7 @@ for(i in 1:length(mi.de.names)){
 }
 
 dfGOde.mi <- do.call(rbind,GOlist.de.mi)
-write.table(dfGOde.mi, "~/Documents/Analyses/Post-zygotic mechanisms/RNAseq/mRNA/DE-analyses/Go-analysis/go-DE-miRNA.txt")
+write.table(dfGOde.mi, "~/go-DE-miRNA.txt")
 
 
 
@@ -145,41 +102,8 @@ for(i in 1:length(names(GOlist.lcv.mi))){
 
 dfGO.lcv.mi <- do.call(rbind,GOlist.lcv.mi)
 
-write.table( dfGO.lcv.mi, "~/Documents/Analyses/Post-zygotic mechanisms/RNAseq/mRNA/DE-analyses/Go-analysis/go-lcv-miRNA.txt")
+write.table( dfGO.lcv.mi, "~/go-lcv-miRNA.txt")
 
-#### GO of mRNA LCV clusters
 
-GOlist.lcv.m <- list()
-
-for(i in 1:length(levels(as.factor(dfcl$mRNACluster)))){
-  sel.genes <-dfcl$gene[dfcl$mRNACluster == i ]
-  sel.genes <- sel.genes[!duplicated(sel.genes)]
-  sel.set <-set1$ID[set1$Gene.name %in% sel.genes]
-  geneList <- factor(as.integer(main.set %in% sel.set))
-  names(geneList) <- main.set
-  if(length(levels(as.factor(geneList))) == 2) {
-    tGOdata <- new("topGOdata",
-                   description = paste("Cluster",i),
-                   ontology = "BP",
-                   allGenes = geneList, 
-                   nodeSize = 10,
-                   annot = annFUN.gene2GO, gene2GO = canada)
-    wtest <- runTest(tGOdata, algorithm = "weight01", statistic = "fisher")
-    
-    gt.de1 <- GenTable(tGOdata, weight01Fisher = wtest, 
-                       orderBy = "weight01Fisher", 
-                       ranksOf = "weight01Fisher",topNodes = 10)
-    GOlist.lcv.m[[i]] <- gt.de1
-  } else {
-    GOlist.lcv.m[[i]] <- matrix("NA", ncol = 7, nrow = 1) }
-}
-names(GOlist.lcv.m) <- c(1:10)
-
-for(i in 1:length(names(GOlist.lcv.m))){
-  GOlist.lcv.m[[i]]$contrast <- rep(names(GOlist.lcv.m)[i], nrow(GOlist.lcv.m[[i]])) 
-}
-
-dfGO.lcv.m <- do.call(rbind,GOlist.lcv.m)
-write.table(dfGO.lcv.m, "~/Documents/Analyses/Post-zygotic mechanisms/RNAseq/mRNA/DE-analyses/Go-analysis/go-lvc.mRNA.txt")
 
 
